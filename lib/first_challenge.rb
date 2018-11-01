c2f4e904ee29f1ad925cafdb710d5d6a7f04bd45
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-01 03:08:49 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
  contacts.delete_if do |contribute, value|
-   :favorite_icecream_flavors == "strawberry"
+   contribute == "strawberry"
   end
   #remember to return your newly altered contacts hash!
   contacts
